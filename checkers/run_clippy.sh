0b6a1c8f1d61972c542c9dc853f184ce2a185503
Hiroki Tokunaga <tokusan441@gmail.com>
2021-04-07 13:44:53 +0900

    chore: use the target `x86_64-unknown-linux-gnu` for running clippy
    It is costly to install MinGW toolchains.

--- checkers/run_clippy.sh
+++ checkers/run_clippy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find . -name Cargo.toml -printf '%h\n'|xargs -P 2 -I {} sh -c "cd {} && cargo clippy -- -D clippy::pedantic -D clippy::all || exit 255"
+find . -name Cargo.toml -printf '%h\n'|xargs -P 2 -I {} sh -c "cd {} && cargo clippy --target=x86_64-unknown-linux-gnu -- -D clippy::pedantic -D clippy::all || exit 255"
 
