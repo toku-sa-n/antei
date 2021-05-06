62b65225f4220c594c385cab2a069c18f7608d13
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-06 15:06:39 +0900

    chore: remove the `run_clippy` script

--- checkers/run_clippy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-find . -name Cargo.toml -printf '%h\n'|xargs -P 2 -I {} sh -c "cd {} && cargo clippy --target=x86_64-unknown-linux-gnu -- -D clippy::pedantic -D clippy::all || exit 255"
