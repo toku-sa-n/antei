dce4b7cecde98e1cdfd20363c1f3efe329659591
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-07 00:13:38 +0900

    feat: implement `Debug` for `SystemTable`

--- bootloader/stable_uefi/src/system_table.rs
+++ bootloader/stable_uefi/src/system_table.rs
@@ -1,4 +1,5 @@
 use crate::service;
+use core::fmt;
 use r_efi::efi;
 
 #[repr(transparent)]
@@ -9,5 +10,10 @@ impl SystemTable {
         // SAFETY: `SystemTable` is created only from the argument of `efi_main`. We must trust the
         // argument is a valid pointer.
         service::Boot(unsafe { &*self.0.boot_services })
+    }
+}
+impl fmt::Debug for SystemTable {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("SystemTable").finish()
     }
 }
