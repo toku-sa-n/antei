5af157d30bbb380bebec1a37ea1f189ffbacd93c
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-07 00:11:18 +0900

    refactor: remove an unused lifetime

--- bootloader/stable_uefi/src/service/boot.rs
+++ bootloader/stable_uefi/src/service/boot.rs
@@ -2,7 +2,7 @@ use core::fmt;
 use r_efi::efi;
 
 pub struct Boot<'a>(pub(crate) &'a efi::BootServices);
-impl<'a> fmt::Debug for Boot<'a> {
+impl fmt::Debug for Boot<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Boot").finish()
     }
