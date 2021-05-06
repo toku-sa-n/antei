b98534b38744ce81dbd59f532880d72152605674
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-07 00:12:50 +0900

    chore: add `must_use`

--- bootloader/stable_uefi/src/system_table.rs
+++ bootloader/stable_uefi/src/system_table.rs
@@ -4,6 +4,7 @@ use r_efi::efi;
 #[repr(transparent)]
 pub struct SystemTable(efi::SystemTable);
 impl SystemTable {
+    #[must_use]
     pub fn boot_services(&self) -> service::Boot<'_> {
         // SAFETY: `SystemTable` is created only from the argument of `efi_main`. We must trust the
         // argument is a valid pointer.
