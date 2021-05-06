b2d23ec23b53f64e6afbce20d4551e459d69ae8d
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-07 00:09:40 +0900

    feat: implement `Debug` for `Boot`

--- bootloader/stable_uefi/src/service/boot.rs
+++ bootloader/stable_uefi/src/service/boot.rs
@@ -1,3 +1,9 @@
+use core::fmt;
 use r_efi::efi;
 
 pub struct Boot<'a>(pub(crate) &'a efi::BootServices);
+impl<'a> fmt::Debug for Boot<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Boot").finish()
+    }
+}
