ee57aa73fb9c2beebd83c2535a5c358bb36d98a2
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-07 12:04:10 +0900

    chore: avoid implementing `Copy` for `Handle`

--- bootloader/stable_uefi/src/handle.rs
+++ bootloader/stable_uefi/src/handle.rs
@@ -2,6 +2,7 @@ use core::fmt;
 use r_efi::efi;
 
 #[repr(transparent)]
+#[allow(missing_copy_implementations)]
 pub struct Handle(efi::Handle);
 impl fmt::Debug for Handle {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
