fa2cc3988da1a1b0f4419d1f80a02e553193a6cc
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-07 11:48:17 +0900

    feat: define `SimpleTextOutput::output_string`

--- bootloader/stable_uefi/src/protocol/console.rs
+++ bootloader/stable_uefi/src/protocol/console.rs
@@ -2,6 +2,11 @@ use core::fmt;
 use r_efi::protocols::simple_text_output;
 
 pub struct SimpleTextOutput<'a>(pub(crate) &'a mut simple_text_output::Protocol);
+impl<'a> SimpleTextOutput<'a> {
+    pub fn output_string(&mut self, buf: &mut [u16]) {
+        (self.0.output_string)(self.0, buf.as_mut_ptr());
+    }
+}
 impl fmt::Debug for SimpleTextOutput<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("SimpleTextOutput").finish()
