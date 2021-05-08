08a4cf8348ccafbbb455e7f852e52c35559c531d
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-08 20:12:04 +0900

    refactor: limit the visibility of the internal of `Boot`
    To prevent the mutation of the internal value.

--- bootloader/uefi_wrapper/src/service/boot.rs
+++ bootloader/uefi_wrapper/src/service/boot.rs
@@ -1,7 +1,12 @@
 use core::fmt;
 use r_efi::efi;
 
-pub struct Boot<'a>(pub(crate) &'a mut efi::BootServices);
+pub struct Boot<'a>(&'a mut efi::BootServices);
+impl<'a> Boot<'a> {
+    pub(crate) fn new(bs: &'a mut efi::BootServices) -> Self {
+        Self(bs)
+    }
+}
 impl fmt::Debug for Boot<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Boot").finish()
