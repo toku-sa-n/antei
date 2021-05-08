b74d590bd5e068c0569335960b27b161897c4bac
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-08 20:25:12 +0900

    refactor: limit the visibility of the internal of `Boot` (#22)
    To prevent the mutation of the internal value.

--- bootloader/uefi_wrapper/src/system_table.rs
+++ bootloader/uefi_wrapper/src/system_table.rs
@@ -10,7 +10,7 @@ impl SystemTable {
     pub fn boot_services(&mut self) -> service::Boot<'_> {
         // SAFETY: `SystemTable` is created only from the argument of `efi_main`. We must trust the
         // argument is a valid pointer.
-        service::Boot(unsafe { &mut *self.0.boot_services })
+        service::Boot::new(unsafe { &mut *self.0.boot_services })
     }
 
     #[must_use]
