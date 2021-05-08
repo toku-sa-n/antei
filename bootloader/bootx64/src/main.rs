1926b2a71703893121fc5f1187f434d86b8b1be6
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-08 09:43:54 +0900

    chore: rewrite with `write!`

--- bootloader/bootx64/src/main.rs
+++ bootloader/bootx64/src/main.rs
@@ -4,24 +4,18 @@
 // For `memcpy`.
 extern crate rlibc as _;
 
+use core::fmt::Write;
 use core::panic::PanicInfo;
 use uefi_wrapper as uefi;
 
 #[no_mangle]
 pub extern "win64" fn efi_main(_: uefi::Handle, mut st: uefi::SystemTable) -> ! {
     let mut stdout = st.con_out();
-    let string = "hello world".as_bytes();
-    let mut buf = [0_u16; 32];
-
-    for i in 0..string.len() {
-        buf[i] = string[i].into();
-    }
 
     let s = stdout.reset_without_extension();
     s.expect("Failed to reset the console.");
     loop {
-        let s = stdout.output_string(&mut buf);
-        s.expect("Failed to print a string.");
+        writeln!(stdout, "hello world").unwrap();
     }
 }
 
