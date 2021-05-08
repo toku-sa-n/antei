be87c5613df3a6a4af37f194eef4d63e9fe4edd5
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-09 00:32:01 +0900

    fix: `SystemTable` must have a pointer, not the actual struct (#23)
    * fix: `SystemTable` must have a pointer, not the actual struct
    
    * chore: deny implementing `Copy` for `SystemTable`

--- bootloader/uefi_wrapper/src/system_table.rs
+++ bootloader/uefi_wrapper/src/system_table.rs
@@ -4,20 +4,21 @@ use core::fmt;
 use r_efi::efi;
 
 #[repr(transparent)]
-pub struct SystemTable(efi::SystemTable);
+#[allow(missing_copy_implementations)]
+pub struct SystemTable(*mut efi::SystemTable);
 impl SystemTable {
     #[must_use]
     pub fn boot_services(&mut self) -> service::Boot<'_> {
         // SAFETY: `SystemTable` is created only from the argument of `efi_main`. We must trust the
         // argument is a valid pointer.
-        service::Boot::new(unsafe { &mut *self.0.boot_services })
+        service::Boot::new(unsafe { &mut *(*self.0).boot_services })
     }
 
     #[must_use]
     pub fn con_out(&mut self) -> console::SimpleTextOutput<'_> {
         // SAFETY: `SystemTable` is created only from the argument of `efi_main`. We must trust the
         // argument is a valid pointer.
-        console::SimpleTextOutput(unsafe { &mut *self.0.con_out })
+        console::SimpleTextOutput(unsafe { &mut *(*self.0).con_out })
     }
 }
 impl fmt::Debug for SystemTable {
