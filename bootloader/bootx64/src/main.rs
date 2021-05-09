a2989fc1ba15037991d2671f271d707a670de434
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-09 23:55:47 +0900

    chore: print the preferred resolution

--- bootloader/bootx64/src/main.rs
+++ bootloader/bootx64/src/main.rs
@@ -7,13 +7,16 @@ extern crate rlibc as _;
 use core::panic::PanicInfo;
 use log::info;
 use uefi_global as uefi;
+use uefi_global::edid;
 
 #[no_mangle]
 pub extern "win64" fn efi_main(h: uefi::Handle, st: uefi::SystemTable) -> ! {
     uefi_global::init(h, st);
 
+    info!("Preferred resolution: {:?}", edid::preferred_resolution());
+
     loop {
-        info!("hello world");
+        x86_64::instructions::hlt();
     }
 }
 
