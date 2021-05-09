0e08327754be27b3a3c1e008e267c6cb70c419ab
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-09 17:55:21 +0900

    chore: remove iterator method
    Not useful to use in `uefi_global`.

--- bootloader/uefi_wrapper/src/protocol/console/graphics_output.rs
+++ bootloader/uefi_wrapper/src/protocol/console/graphics_output.rs
@@ -12,17 +12,13 @@ pub use r_efi::protocols::graphics_output::ModeInformation;
 #[repr(transparent)]
 pub struct GraphicsOutput(*mut graphics_output::Protocol);
 impl GraphicsOutput {
-    pub fn iter_mode(&mut self) -> impl Iterator<Item = ModeInformation> + '_ {
-        ModeIter::from(self)
-    }
-
     pub fn set_mode(&mut self, mode_number: u32) -> Result<()> {
         let s = (self.get_mut().set_mode)(self.0, mode_number);
 
         result::from_value_and_status((), s)
     }
 
-    fn query_mode(&mut self, mode_number: u32) -> Result<ModeInformation> {
+    pub fn query_mode(&mut self, mode_number: u32) -> Result<ModeInformation> {
         let mut size = MaybeUninit::uninit();
         let mut info = MaybeUninit::uninit();
 
@@ -41,17 +37,6 @@ impl GraphicsOutput {
         result::from_closure_and_status(|| unsafe { *info }, s)
     }
 
-    fn max_mode(&self) -> u32 {
-        let m = self.get_ref().mode;
-
-        unsafe { (*m).max_mode }
-    }
-
-    fn get_ref(&self) -> &graphics_output::Protocol {
-        // SAFETY: This type is created only through `Boot::locate_protocol_without_regstration`.
-        unsafe { &*self.0 }
-    }
-
     fn get_mut(&mut self) -> &mut graphics_output::Protocol {
         // SAFETY: This type is created only through `Boot::locate_protocol_without_regstration`.
         unsafe { &mut *self.0 }
@@ -64,28 +49,4 @@ impl fmt::Debug for GraphicsOutput {
 }
 unsafe impl Protocol for GraphicsOutput {
     const GUID: efi::Guid = graphics_output::PROTOCOL_GUID;
-}
-
-struct ModeIter<'a> {
-    gop: &'a mut GraphicsOutput,
-    i: u32,
-}
-impl<'a> Iterator for ModeIter<'a> {
-    type Item = ModeInformation;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.i >= self.gop.max_mode() {
-            None
-        } else {
-            let m = self.gop.query_mode(self.i);
-            self.i += 1;
-
-            m.ok().or_else(|| self.next())
-        }
-    }
-}
-impl<'a> From<&'a mut GraphicsOutput> for ModeIter<'a> {
-    fn from(gop: &'a mut GraphicsOutput) -> Self {
-        Self { gop, i: 0 }
-    }
 }
