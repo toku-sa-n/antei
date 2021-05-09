20442b198b27b5da2880d4d044809e8a2ac391e4
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-09 11:47:34 +0900

    feat: define `locate_protocol_without_registration`

--- bootloader/uefi_wrapper/src/service/boot.rs
+++ bootloader/uefi_wrapper/src/service/boot.rs
@@ -1,8 +1,24 @@
+use crate::result;
+use crate::result::Result;
 use crate::system_table::SystemTable;
+use core::ffi::c_void;
 use core::fmt;
+use core::mem::MaybeUninit;
+use core::ptr;
 use r_efi::efi;
 
 pub struct Boot<'a>(&'a mut efi::BootServices, &'a mut SystemTable);
+impl<'a> Boot<'a> {
+    pub fn locate_protocol_without_registration(
+        &mut self,
+        mut guid: efi::Guid,
+    ) -> Result<*mut c_void> {
+        let mut protocol = MaybeUninit::uninit();
+        let s = (self.0.locate_protocol)(&mut guid, ptr::null_mut(), protocol.as_mut_ptr());
+
+        result::from_value_and_status(unsafe { protocol.assume_init() }, s)
+    }
+}
 impl<'a> From<&'a mut SystemTable> for Boot<'a> {
     fn from(s: &'a mut SystemTable) -> Self {
         let s_ptr = s.get_ptr();
