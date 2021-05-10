b627d538ef27599a2bc9e412b83b83f38407634a
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-10 10:49:50 +0900

    chore: add an `Err` note

--- bootloader/uefi_global/src/edid.rs
+++ bootloader/uefi_global/src/edid.rs
@@ -1,6 +1,9 @@
 use uefi_wrapper::protocol::console::edid;
 use uefi_wrapper::result::Result;
 
+/// # Errors
+///
+/// This method returns an `Err` value if EDID Discovered protocol is not found.
 pub fn preferred_resolution() -> Result<Option<(u32, u32)>> {
     let mut st = crate::system_table();
     let bs = st.boot_services();
