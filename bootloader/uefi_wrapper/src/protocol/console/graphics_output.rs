74b065453ea0debf67d62033d3165293ad1415e2
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-17 21:35:33 +0900

    feat(uefi_wrapper): implement `Debug` for `GraphicsOutput`

--- bootloader/uefi_wrapper/src/protocol/console/graphics_output.rs
+++ bootloader/uefi_wrapper/src/protocol/console/graphics_output.rs
@@ -1,3 +1,9 @@
+use core::fmt;
 use r_efi::efi::protocols::graphics_output;
 
 pub struct GraphicsOutput(graphics_output::Protocol);
+impl fmt::Debug for GraphicsOutput {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("GraphicsOutput").finish()
+    }
+}
