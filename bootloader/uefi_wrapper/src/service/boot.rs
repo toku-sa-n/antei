1e7d872b4cda6640277c7cdce72c895a1db749d8
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-18 11:22:25 +0900

    feat(uefi_wrapper): define `Boot::locate_protocol_without_registration`

--- bootloader/uefi_wrapper/src/service/boot.rs
+++ bootloader/uefi_wrapper/src/service/boot.rs
@@ -1,7 +1,34 @@
+use crate::result;
 use core::fmt;
+use core::mem;
+use core::ptr;
 use r_efi::efi;
 
 pub struct Boot<'a>(&'a mut efi::BootServices, &'a mut crate::SystemTable);
+impl<'a> Boot<'a> {
+    /// To avoid to create multiple pointers to the same protocol (which is potentially dangerous
+    /// as it may create multiple mutable references to the same object), this method generates
+    /// [`WithProtocol`].
+    pub fn locate_protocol_without_registration<P: crate::Protocol>(
+        self,
+    ) -> crate::Result<WithProtocol<'a, P>> {
+        let mut protocol = mem::MaybeUninit::uninit();
+        let mut g = P::GUID;
+        let r = (self.0.locate_protocol)(&mut g, ptr::null_mut(), protocol.as_mut_ptr());
+
+        result::from_closure_and_status(r, || {
+            // SAFETY: `locate_protocol` initializes `protocol`.
+            let protocol = unsafe { protocol.assume_init() }.cast::<P>();
+
+            // SAFETY: On success, `protocol` is initialized to the pointer to the protocol.
+            //
+            // There is no mutable references to the protocol as there is no way to create it
+            // without this method.
+            let protocol = unsafe { &mut *protocol };
+            WithProtocol::new(protocol, self)
+        })
+    }
+}
 impl<'a> From<&'a mut crate::SystemTable> for Boot<'a> {
     fn from(s: &'a mut crate::SystemTable) -> Self {
         let s_ptr = s.get_ptr();
