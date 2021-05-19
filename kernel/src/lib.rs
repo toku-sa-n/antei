b8b2eb7d37a324902d196846dfd861fac90996fd
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-19 15:33:52 +0900

    refactor(kernel): remove unnecessary functions

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -6,14 +6,3 @@ use core::panic;
 fn panic(_: &panic::PanicInfo<'_>) -> ! {
     loop {}
 }
-
-#[no_mangle]
-fn __libc_start_main(main: fn() -> isize) {
-    main();
-}
-
-#[no_mangle]
-fn __libc_csu_init() {}
-
-#[no_mangle]
-fn __libc_csu_fini() {}
