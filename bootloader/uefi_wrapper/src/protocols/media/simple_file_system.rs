b1a0d324a0ceb6b8cf5875e15dfab34e94684c9f
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-19 16:53:05 +0900

    feat(uefi_wrapper): implement `Protocol` for `SimpleFileSystem`

--- bootloader/uefi_wrapper/src/protocols/media/simple_file_system.rs
+++ bootloader/uefi_wrapper/src/protocols/media/simple_file_system.rs
@@ -1,4 +1,8 @@
+use r_efi::efi;
 use r_efi::efi::protocols::simple_file_system;
 
 #[repr(transparent)]
 pub struct SimpleFileSystem(simple_file_system::Protocol);
+unsafe impl crate::Protocol for SimpleFileSystem {
+    const GUID: efi::Guid = simple_file_system::PROTOCOL_GUID;
+}
