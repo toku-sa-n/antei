7ac6fc33dc4b1b8879e93d43b783557baa791ddb
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-22 21:58:16 +0900

    chore(uefi_wrapper): relax lifetimes

--- bootloader/uefi_wrapper/src/protocols/media/file.rs
+++ bootloader/uefi_wrapper/src/protocols/media/file.rs
@@ -11,7 +11,7 @@ pub struct File<'a> {
     fs: &'a mut SimpleFileSystem,
 }
 impl<'a> File<'a> {
-    pub fn open_read_only(&'a mut self, name: &'static str) -> crate::Result<File<'a>> {
+    pub fn open_read_only(&'a mut self, name: &str) -> crate::Result<File<'a>> {
         if name_too_long(name) {
             Err(Status::INVALID_PARAMETER.into())
         } else {
@@ -46,7 +46,7 @@ impl<'a> File<'a> {
         Self { handler, fs }
     }
 
-    fn open_read_only_unchecked(&'a mut self, name: &'static str) -> crate::Result<File<'a>> {
+    fn open_read_only_unchecked(&'a mut self, name: &str) -> crate::Result<File<'a>> {
         let mut name = name_to_u16_array(name);
         let mut new_handler = mem::MaybeUninit::uninit();
 
@@ -76,7 +76,7 @@ impl<'a> File<'a> {
     }
 }
 
-fn name_to_u16_array(name: &'static str) -> [u16; FAT_MAX_NAME + 1] {
+fn name_to_u16_array(name: &str) -> [u16; FAT_MAX_NAME + 1] {
     let mut buf = [0; FAT_MAX_NAME + 1];
     let r = ucs2::encode(name, &mut buf);
 
