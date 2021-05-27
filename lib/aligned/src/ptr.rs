b5936d901189b1c9c6b1805c39dca95e22587e76
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-27 14:38:34 +0900

    feat(aligned_ptr): define `read`

--- lib/aligned/src/ptr.rs
+++ lib/aligned/src/ptr.rs
@@ -3,6 +3,53 @@ use crate::Error;
 
 const ERR_MSG: &str = "Pointer is either null or not aligned.";
 
+/// Reads a value the pointer `p` points with [`core::ptr::read`].
+///
+/// # Safety
+///
+/// The pointer `p` must follow these rules:
+///
+/// - It must be dereferencable as defined in [Rust's documentation](https://doc.rust-lang.org/std/ptr/index.html#safety).
+/// - It must point to the initialized instance of T.
+///
+/// Also, the caller must follow Rust's aliasing rule. The caller must not create both immutable
+/// and mutable references, or multiple mutable references to the same object.
+///
+/// # Panics
+///
+/// This method panics if `p` is either null or not aligned correctly.
+pub unsafe fn read<T>(p: *const T) -> T {
+    try_read(p).expect(ERR_MSG)
+}
+
+/// Reads a value the pointer `p` points with [`core::ptr::read`].
+///
+/// # Safety
+///
+/// The pointer `p` must follow these rules:
+///
+/// - It must be dereferencable as defined in [Rust's documentation](https://doc.rust-lang.org/std/ptr/index.html#safety).
+/// - It must point to the initialized instance of T.
+///
+/// Also, the caller must follow Rust's aliasing rule. The caller must not create both immutable
+/// and mutable references, or multiple mutable references to the same object.
+///
+/// # Errors
+///
+/// This method may return an error:
+///
+/// - [`Error::Null`] - `p` is null.
+/// - [`Error::NotAligned`] - `p` is not aligned correctly.
+pub unsafe fn try_read<T>(p: *const T) -> Result<T, Error> {
+    if p.is_null() {
+        Err(Error::Null)
+    } else if is_aligned(p) {
+        Ok(p.read())
+    } else {
+        Err(Error::NotAligned)
+    }
+}
+
 /// Gets a value the pointer `p` points by dereferencing it.
 ///
 /// # Safety
