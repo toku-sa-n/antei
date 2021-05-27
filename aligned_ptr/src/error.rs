491ac6d2d89503e01c8efe53377d3dd0ced26347
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-27 10:13:33 +0900

    feat(aligned_ptr): define `as_ref`

--- aligned_ptr/src/error.rs
+++ aligned_ptr/src/error.rs
@@ -1,6 +1,7 @@
 //! Errors.
 
 /// Errors returned by functions of this crate.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Error {
     /// The passed pointer is null.
     Null,
--- aligned_ptr/src/lib.rs
+++ aligned_ptr/src/lib.rs
@@ -1,1 +1,55 @@
 pub mod error;
+
+use core::mem;
+pub use error::Error;
+
+/// Converts a pointer to a reference.
+///
+/// # Safety
+///
+/// The pointer `p` must follow these rules:
+///
+/// - It must be dereferencable as defined in [Rust's documentation](https://doc.rust-lang.org/std/ptr/index.html#safety).
+/// - It must point to the initialized instance of T.
+///
+/// Also, the caller must follow Rust's aliasing rule. The caller must not create both immutable
+/// and mutable references to the same object simultaneously.
+///
+/// # Panics
+///
+/// This method panics if `p` is either null or not aligned correctly.
+pub unsafe fn as_ref<'a, T>(p: *const T) -> &'a T {
+    try_as_ref(p).expect("Pointer is either null or not aligned.")
+}
+
+/// Converts a pointer to a reference.
+///
+/// # Safety
+///
+/// The pointer `p` must follow these rules:
+///
+/// - It must be dereferencable as defined in [Rust's documentation](https://doc.rust-lang.org/std/ptr/index.html#safety).
+/// - It must point to the initialized instance of T.
+///
+/// Also, the caller must follow Rust's aliasing rule. The caller must not create both immutable
+/// and mutable references to the same object simultaneously.
+///
+/// # Errors
+///
+/// This method may return an error:
+///
+/// - [`Error::Null`] - `p` is null.
+/// - [`Error::NotAligned`] - `p` is not aligned correctly.
+pub unsafe fn try_as_ref<'a, T>(p: *const T) -> Result<&'a T, crate::Error> {
+    if p.is_null() {
+        Err(Error::Null)
+    } else if is_aligned(p) {
+        Ok(&*p)
+    } else {
+        Err(Error::NotAligned)
+    }
+}
+
+fn is_aligned<T>(p: *const T) -> bool {
+    p as usize % mem::align_of::<T>() == 0
+}
