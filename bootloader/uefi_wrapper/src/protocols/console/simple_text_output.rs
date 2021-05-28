ff99196af35922de7cb8d9349d86c775f4b12682
Hiroki Tokunaga <tokusan441@gmail.com>
2021-05-28 15:11:15 +0900

    chore(uefi_wrapper): remove unnecessary sentences

--- bootloader/uefi_wrapper/src/protocols/console/simple_text_output.rs
+++ bootloader/uefi_wrapper/src/protocols/console/simple_text_output.rs
@@ -27,8 +27,7 @@ impl<'a> From<&'a mut SystemTable> for SimpleTextOutput<'a> {
         let s_ptr = s.get_ptr();
 
         // SAFETY: `SystemTable` is created only from the argument of `efi_main`, so the mutable
-        // reference to `SimpleTextOutput` is created only once. Also, we must trust the
-        // pointer is valid.
+        // reference to `SimpleTextOutput` is created only once.
         let st = unsafe { aligned_ptr::as_mut(s_ptr) };
         let sto = unsafe { aligned_ptr::as_mut(st.con_out) };
         Self(sto, s)
