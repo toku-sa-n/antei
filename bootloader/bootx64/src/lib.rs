10a111a20ad228976c201c05d70856812e0ffbe2
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-01 17:15:08 +0900

    feat(bootx64): implement `expect_some`

--- bootloader/bootx64/src/lib.rs
+++ bootloader/bootx64/src/lib.rs
@@ -5,6 +5,7 @@ pub mod gop;
 pub mod io;
 pub mod panic;
 
+use core::fmt;
 use uefi_wrapper::protocols::console;
 use uefi_wrapper::service;
 
@@ -25,6 +26,15 @@ impl SystemTable {
             Some(val) => val,
             None => {
                 uefi_panic!(self, "{}", msg);
+            }
+        }
+    }
+
+    pub fn expect_ok<T, E: fmt::Debug>(&mut self, result: Result<T, E>, msg: &str) -> T {
+        match result {
+            Ok(val) => val,
+            Err(e) => {
+                uefi_panic!(self, "{}: {:?}", msg, e);
             }
         }
     }
