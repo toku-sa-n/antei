4dc2483603a97767bb6b1064e81b47118e02f723
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-01 21:17:06 +0900

    feat(aligned): implement `slice::from_raw_parts_mut`

---
+++ lib/aligned/src/slice.rs
@@ -0,0 +1,28 @@
+use crate::is_aligned;
+use crate::Error;
+
+/// # Safety
+///
+/// The caller must follow the rules of caling [`core::slice::from_raw_parts_mut`] except the
+/// alignment requirements.
+pub unsafe fn from_raw_parts_mut<'a, T>(data: *mut T, len: usize) -> &'a mut [T] {
+    let r = try_from_raw_parts_mut(data, len);
+    r.expect("Pointer is either null or not aligned.")
+}
+
+/// # Safety
+///
+/// The caller must follow the rules of calling [`core::slice::from_raw_parts_mut`] except the
+/// alignment requirements.
+pub unsafe fn try_from_raw_parts_mut<'a, T>(
+    data: *mut T,
+    len: usize,
+) -> Result<&'a mut [T], Error> {
+    if data.is_null() {
+        Err(Error::Null)
+    } else if is_aligned(data) {
+        Ok(core::slice::from_raw_parts_mut(data, len))
+    } else {
+        Err(Error::NotAligned)
+    }
+}
