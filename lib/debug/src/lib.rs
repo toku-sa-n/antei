7463ffcfdb43d535428f756acc84ea4c891f768c
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-03 00:00:38 +0900

    chore(debug): change the return type to `!`
    To find where the debug point is.

--- lib/debug/src/lib.rs
+++ lib/debug/src/lib.rs
@@ -1,10 +1,10 @@
 #![no_std]
 
 extern "C" {
-    fn asm_stop();
+    fn asm_stop() -> !;
 }
 
-pub fn stop() {
+pub fn stop() -> ! {
     // SAFETY: `asm_stop` just loops infinitely.
     unsafe { asm_stop() }
 }
