5cecd06353cbde4d8454d8d7bc217fb66e702b24
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-04 16:51:09 +0900

    chore(bootx64): define `Mapper::map`
    To map the kernel.

--- bootloader/bootx64/src/mapper.rs
+++ bootloader/bootx64/src/mapper.rs
@@ -1,7 +1,10 @@
 use crate::Allocator;
 use aligned::ptr;
+use x86_64::structures::paging::PageTableFlags;
+use x86_64::structures::paging::PhysFrame;
 use x86_64::structures::paging::RecursivePageTable;
 use x86_64::structures::paging::{self, Size4KiB};
+use x86_64::structures::paging::{Mapper as MapperTrait, Page};
 use x86_64::VirtAddr;
 
 const RECURSIVE_PAGING: VirtAddr = VirtAddr::new_truncate(0xffff_ff7f_bfdf_e000);
@@ -21,5 +24,16 @@ impl<'a> Mapper<'a> {
         let mapper = mapper.expect("The recursive entry is not enabled.");
 
         Self { allocator, mapper }
+    }
+
+    /// # Safety
+    ///
+    /// See [`x86_64::structures::paging::Mapper`].
+    unsafe fn map(&mut self, page: Page<Size4KiB>, frame: PhysFrame) {
+        let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+
+        let flush = unsafe { self.mapper.map_to(page, frame, flags, self.allocator) };
+        let flush = flush.expect("Failed to map a page.");
+        flush.flush();
     }
 }
