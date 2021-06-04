192c0188da9305c92621932a7dc4ca51881d2be7
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-04 19:26:39 +0900

    chore(bootx64): add a temporary trait impl
    `ElfLoader` for `Mapper`.

--- bootloader/bootx64/src/elf.rs
+++ bootloader/bootx64/src/elf.rs
@@ -10,3 +10,21 @@ impl<'a> Mapper<'a> {
         Self { allocator, binary }
     }
 }
+impl ElfLoader for Mapper<'_> {
+    fn allocate(&mut self, load_headers: elfloader::LoadableHeaders) -> Result<(), &'static str> {
+        todo!()
+    }
+
+    fn load(
+        &mut self,
+        flags: elfloader::Flags,
+        base: elfloader::VAddr,
+        region: &[u8],
+    ) -> Result<(), &'static str> {
+        todo!()
+    }
+
+    fn relocate(&mut self, entry: &elfloader::Rela<elfloader::P64>) -> Result<(), &'static str> {
+        todo!()
+    }
+}
