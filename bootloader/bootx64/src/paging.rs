bf76e68c841894abb22cd731c9855a2110eec256
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-04 11:58:16 +0900

    chore(bootx64): define `pml4_addr`
    To edit page tables.

--- bootloader/bootx64/src/paging.rs
+++ bootloader/bootx64/src/paging.rs
@@ -1,5 +1,7 @@
 use x86_64::registers::control::Cr0;
 use x86_64::registers::control::Cr0Flags;
+use x86_64::registers::control::Cr3;
+use x86_64::PhysAddr;
 
 pub fn enable_write_protect() {
     unsafe { Cr0::update(|flags| flags.insert(Cr0Flags::WRITE_PROTECT)) }
@@ -7,4 +9,9 @@ pub fn enable_write_protect() {
 
 pub fn disable_write_protect() {
     unsafe { Cr0::update(|flags| flags.remove(Cr0Flags::WRITE_PROTECT)) }
+}
+
+pub fn pml4_addr() -> PhysAddr {
+    let f = Cr3::read().0;
+    f.start_address()
 }
