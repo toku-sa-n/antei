f921d678fa6708daabf417eb59866b742851b5f9
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-04 21:42:54 +0900

    refactor(bootx64): move the definition of `SystemTable` (#68)
    * chore(bootx64): create `system_table.rs`
    
    To move the definition of `SystemTable` to this file.
    
    * refactor(bootx64): move a struct definition
    
    `lib.rs` should contain the minimum lines.

---
+++ bootloader/bootx64/src/system_table.rs
@@ -0,0 +1,39 @@
+use crate::uefi_panic;
+use core::fmt;
+use uefi_wrapper::service;
+use uefi_wrapper::{protocols::console, service::boot};
+
+#[repr(transparent)]
+#[derive(Debug)]
+pub struct SystemTable(uefi_wrapper::SystemTable);
+impl SystemTable {
+    pub fn boot_services(&mut self) -> service::Boot<'_> {
+        self.0.boot_services()
+    }
+
+    pub fn con_out(&mut self) -> console::SimpleTextOutput<'_> {
+        self.0.con_out()
+    }
+
+    /// # Panics
+    ///
+    /// This method panics if `result` is [`Err`].
+    pub fn expect_ok<T, E: fmt::Debug>(&mut self, result: Result<T, E>, msg: &str) -> T {
+        match result {
+            Ok(val) => val,
+            Err(e) => {
+                uefi_panic!(self, "{}: {:?}", msg, e);
+            }
+        }
+    }
+
+    pub(crate) fn exit_boot_services(
+        self,
+        image_handler: uefi_wrapper::Handle,
+        map_key: boot::MapKey,
+    ) -> uefi_wrapper::Result<(), (Self, uefi_wrapper::Handle)> {
+        let r = self.0.exit_boot_services(image_handler, map_key);
+
+        r.map_err(|e| e.map_value(|(st, h)| (Self(st), h)))
+    }
+}
