cbaf1925d54cc7b7fa267444e50c4ff14fcd8c67
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-04 10:40:36 +0900

    chore(bootx64): add a trait implementation
    This commit implements `FrameAllocator<Size4KiB> for `Allocator`.

--- bootloader/bootx64/src/allocator.rs
+++ bootloader/bootx64/src/allocator.rs
@@ -1,5 +1,10 @@
 use uefi_wrapper::service::boot::MemoryDescriptor;
 use uefi_wrapper::service::boot::MemoryType;
+use x86_64::structures::paging::{PageSize, Size4KiB};
+use x86_64::{
+    structures::paging::{FrameAllocator, PhysFrame},
+    PhysAddr,
+};
 
 struct Allocator<'a> {
     mem_map: &'a mut [MemoryDescriptor],
@@ -13,5 +18,24 @@ impl<'a> Allocator<'a> {
         self.mem_map
             .iter_mut()
             .filter(|x| x.r#type == MemoryType::ConventionalMemory as u32)
+    }
+}
+unsafe impl FrameAllocator<Size4KiB> for Allocator<'_> {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        for d in self.iter_mut_conventional() {
+            if d.number_of_pages > 0 {
+                let f = d.physical_start;
+                let f = PhysAddr::new(f);
+                let f = PhysFrame::from_start_address(f);
+                let f = f.expect("The address is not page-aligned.");
+
+                d.number_of_pages -= 1;
+                d.physical_start += Size4KiB::SIZE;
+
+                return Some(f);
+            }
+        }
+
+        None
     }
 }
