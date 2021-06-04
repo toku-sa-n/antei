8d1cd47da3f469b9dd9287d1f33b383fb5f72e95
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-04 10:17:29 +0900

    chore(bootx64): define a method to `Mapper`
    `iter_mut_conventional` for iterating usable memory ranges.

--- bootloader/bootx64/src/mapper.rs
+++ bootloader/bootx64/src/mapper.rs
@@ -1,4 +1,5 @@
 use uefi_wrapper::service::boot::MemoryDescriptor;
+use uefi_wrapper::service::boot::MemoryType;
 
 struct Mapper<'a> {
     mem_map: &'a mut [MemoryDescriptor],
@@ -6,5 +7,11 @@ struct Mapper<'a> {
 impl<'a> Mapper<'a> {
     fn new(mem_map: &'a mut [MemoryDescriptor]) -> Self {
         Self { mem_map }
+    }
+
+    fn iter_mut_conventional(&mut self) -> impl Iterator<Item = &mut MemoryDescriptor> {
+        self.mem_map
+            .iter_mut()
+            .filter(|x| x.r#type == MemoryType::ConventionalMemory as u32)
     }
 }
