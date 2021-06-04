fa9d83402ce32a0be7ad1f0dbcaf203886b217b7
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-04 15:43:04 +0900

    chore(bootx64): add a function
    This commit defines `enable_recursive_paging` to use
    `RecursivePageTable`.

--- bootloader/bootx64/src/paging.rs
+++ bootloader/bootx64/src/paging.rs
@@ -1,7 +1,11 @@
+use aligned::ptr;
 use x86_64::registers::control::Cr0;
 use x86_64::registers::control::Cr0Flags;
 use x86_64::registers::control::Cr3;
+use x86_64::structures::paging::PageTable;
+use x86_64::structures::paging::PageTableFlags;
 use x86_64::PhysAddr;
+use x86_64::VirtAddr;
 
 pub fn enable_write_protect() {
     unsafe { Cr0::update(|flags| flags.insert(Cr0Flags::WRITE_PROTECT)) }
@@ -9,6 +13,21 @@ pub fn enable_write_protect() {
 
 pub fn disable_write_protect() {
     unsafe { Cr0::update(|flags| flags.remove(Cr0Flags::WRITE_PROTECT)) }
+}
+
+/// # Safety
+///
+/// This function assumes that the physical and the virtual of the PML4 is the same value.
+pub unsafe fn enable_recursive_paging() {
+    let p = pml4_addr();
+    let v = VirtAddr::new(p.as_u64());
+
+    // SAFETY: The caller must ensure that the physical and virtual addresses of the PML4 is the
+    // same value.
+    let table: &mut PageTable = unsafe { ptr::as_mut(v.as_mut_ptr()) };
+
+    let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+    table[0x510].set_addr(p, flags);
 }
 
 pub fn pml4_addr() -> PhysAddr {
