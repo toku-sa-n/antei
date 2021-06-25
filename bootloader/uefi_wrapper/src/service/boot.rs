0f45c25346b0112bb26ec575f94ae9942a249b06
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-25 11:34:28 +0900

    refactor(boot): use `bool::then` (#77)

--- bootloader/uefi_wrapper/src/service/boot.rs
+++ bootloader/uefi_wrapper/src/service/boot.rs
@@ -95,11 +95,7 @@ impl<'a> Boot<'a> {
         } else {
             Err(crate::Error::from_status_and_value(
                 s.into(),
-                if s == efi::Status::BUFFER_TOO_SMALL {
-                    Some(memory_map_size)
-                } else {
-                    None
-                },
+                (s == efi::Status::BUFFER_TOO_SMALL).then(|| memory_map_size),
             ))
         }
     }
