e6523e251c718f0d99876efa0b57803c2c59f8b0
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-30 15:52:46 +0900

    refactor(bootx64): remove unnecessary processes

--- bootloader/bootx64/src/main.rs
+++ bootloader/bootx64/src/main.rs
@@ -6,17 +6,12 @@
 extern crate rlibc as _;
 
 use bootx64::elf;
-use bootx64::gop;
+use bootx64::fs;
 use bootx64::paging;
-use bootx64::{fs, uefi_println};
 
 #[no_mangle]
 extern "win64" fn efi_main(h: uefi_wrapper::Handle, mut st: bootx64::SystemTable) -> ! {
-    let resolution = gop::set_preferred_resolution(&mut st);
-    uefi_println!(&mut st, "GOP info: {:?}", resolution);
-
     let bytes = fs::locate(&mut st, "kernel");
-    uefi_println!(&mut st, "{:X?}", &bytes[0..8]);
 
     let mmap = bootx64::exit_boot_services_and_return_mmap(h, st);
 
