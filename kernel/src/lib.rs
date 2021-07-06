29ab6f240c803b751331a61a83f5a6f8184c0de2
Hiroki Tokunaga <tokusan441@gmail.com>
2021-07-06 22:04:14 +0900

    feat: initialize GDT (#88)

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,6 +1,10 @@
 #![no_std]
 #![deny(unsafe_op_in_unsafe_fn)]
 
+extern crate rlibc as _;
+
+pub mod gdt;
+
 use core::panic;
 
 #[panic_handler]
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -4,8 +4,12 @@
 
 extern crate kernel as _;
 
+use kernel::gdt;
+
 #[no_mangle]
 fn main() {
+    gdt::init();
+
     unsafe {
         loop {
             *(0x334 as *mut u8) = 3_u8;
