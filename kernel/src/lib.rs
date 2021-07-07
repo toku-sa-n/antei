03fe8c3cae118b2ad6b84d141133ac5a1fec1eda
Hiroki Tokunaga <tokusan441@gmail.com>
2021-07-07 15:54:06 +0900

    feat(kernel): print the panic information

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -6,8 +6,11 @@ extern crate rlibc as _;
 pub mod gdt;
 
 use core::panic::PanicInfo;
+use qemu_print::qemu_println;
 
 #[panic_handler]
-fn panic(_: &PanicInfo<'_>) -> ! {
+fn panic(i: &PanicInfo<'_>) -> ! {
+    qemu_println!("{}", i);
+
     loop {}
 }
