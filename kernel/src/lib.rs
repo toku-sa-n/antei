c3c1eea2a83bddc73ec6975e8d25ccfcbf7ad340
toku-sa-n <tokusan441@gmail.com>
2021-07-18 15:51:18 +0900

    chore(kernel): remove an import

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -5,7 +5,7 @@ extern crate rlibc as _;
 
 pub mod gdt;
 
-use {core::panic::PanicInfo, qemu_print::qemu_println, x86_64::instructions::hlt};
+use {core::panic::PanicInfo, qemu_print::qemu_println};
 
 #[cfg(feature = "test_on_qemu")]
 pub fn fini() -> ! {
@@ -15,7 +15,7 @@ pub fn fini() -> ! {
 #[cfg(not(feature = "test_on_qemu"))]
 pub fn fini() -> ! {
     loop {
-        hlt();
+        x86_64::instructions::hlt();
     }
 }
 
