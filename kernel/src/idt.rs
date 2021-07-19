4ae73da389464ab5c46c3b3bcf9bfa111362ab6a
Hiroki Tokunaga <tokusan441@gmail.com>
2021-07-19 11:39:26 +0900

    test(idt): address of IDT

--- kernel/src/idt.rs
+++ kernel/src/idt.rs
@@ -5,4 +5,36 @@ static IDT: Lazy<InterruptDescriptorTable> = Lazy::new(InterruptDescriptorTable:
 
 pub fn init() {
     IDT.load();
+
+    #[cfg(feature = "test_on_qemu")]
+    tests::main();
+}
+
+#[cfg(feature = "test_on_qemu")]
+mod tests {
+    use {
+        super::IDT,
+        x86_64::{instructions::tables, structures::idt::InterruptDescriptorTable, VirtAddr},
+    };
+
+    pub(super) fn main() {
+        assert_idt_address_is_correct();
+    }
+
+    fn assert_idt_address_is_correct() {
+        let idt = idt();
+        let expected_addr = VirtAddr::from_ptr(idt);
+
+        let descriptor_table_ptr = tables::sidt();
+        let actual_base = descriptor_table_ptr.base;
+
+        assert_eq!(
+            expected_addr, actual_base,
+            "The address of the current IDT is not correct."
+        );
+    }
+
+    fn idt<'a>() -> &'a InterruptDescriptorTable {
+        &*IDT
+    }
 }
