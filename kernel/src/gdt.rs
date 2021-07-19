141fd186218e54b7d6592653fd3159ed3d40b9ab
Hiroki Tokunaga <tokusan441@gmail.com>
2021-07-19 11:34:36 +0900

    test(gdt): assert all segments are correctly set (#107)

--- kernel/src/gdt.rs
+++ kernel/src/gdt.rs
@@ -31,6 +31,9 @@ pub fn init() {
     init_gdt();
     lgdt();
     load_segments();
+
+    #[cfg(feature = "test_on_qemu")]
+    tests::main();
 }
 
 fn init_gdt() {
@@ -83,4 +86,70 @@ fn gdt<'a>() -> &'a GlobalDescriptorTable {
 fn selectors<'a>() -> &'a Selectors {
     let selectors = SELECTORS.try_get();
     selectors.expect("`SELECTORS` is not initialized.")
+}
+
+#[cfg(feature = "test_on_qemu")]
+mod tests {
+    use {
+        super::{gdt, selectors},
+        x86_64::{
+            instructions::{
+                segmentation::{Segment, CS, DS, ES, FS, GS, SS},
+                tables,
+            },
+            VirtAddr,
+        },
+    };
+
+    pub(super) fn main() {
+        assert_gdt_address_is_correct();
+        assert_selectors_are_correctly_set();
+    }
+
+    fn assert_gdt_address_is_correct() {
+        let gdt = gdt();
+        let expected_addr = VirtAddr::from_ptr(gdt);
+
+        let descriptor_table_ptr = tables::sgdt();
+        let actual_addr = descriptor_table_ptr.base;
+
+        assert_eq!(
+            expected_addr, actual_addr,
+            "The address of the current GDT is not correct."
+        );
+    }
+
+    fn assert_selectors_are_correctly_set() {
+        let selectors = selectors();
+
+        let code = selectors.kernel_code;
+        let data = selectors.kernel_data;
+
+        macro_rules! assert_segment {
+            ($seg:ident,$correct:expr) => {
+                assert_eq!(
+                    $seg::get_reg(),
+                    $correct,
+                    "Incorrect {}",
+                    core::stringify!($seg)
+                );
+            };
+        }
+
+        macro_rules! code{
+            ($($seg:ident),+)=>{
+                $(assert_segment!($seg,code);)+
+            }
+        }
+
+        macro_rules! data{
+            ($($seg:ident),+)=>{
+                $(assert_segment!($seg,data);)+
+            }
+        }
+
+        code!(CS);
+
+        data!(DS, ES, FS, GS, SS);
+    }
 }
