b4d9840afa355ffc9a2777bd1cc4adc6f1ab3bb3
Hiroki Tokunaga <tokusan441@gmail.com>
2021-07-19 15:40:11 +0900

    feat(boot_info): define `BootInfo`

--- libs/boot_info/src/lib.rs
+++ libs/boot_info/src/lib.rs
@@ -1,7 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![no_std]
+
+const MAGIC_HEADER: u64 = 0x0114_0514_1919_0810;
+const MAGIC_FOOTER: u64 = 0x0334_0072_dead_cafe;
+
+#[repr(C)]
+pub struct BootInfo {
+    magic_header: u64,
+    magic_footer: u64,
+}
+impl BootInfo {
+    pub fn check_vaildity(&self) {
+        assert_eq!(
+            self.magic_header, MAGIC_HEADER,
+            "Invalid boot information header."
+        );
+
+        assert_eq!(
+            self.magic_footer, MAGIC_FOOTER,
+            "Invalid boot information footer."
+        );
     }
 }
