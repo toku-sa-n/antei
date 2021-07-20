c9d2d9c397fe7507e2b2f35ce1e694598e402bc2
toku-sa-n <tokusan441@gmail.com>
2021-07-20 21:34:33 +0900

    refactor(bootx64): integrate `use`s

--- bootx64/src/lib.rs
+++ bootx64/src/lib.rs
@@ -18,8 +18,5 @@ pub mod system_table;
 pub(crate) type NumOfPages<T = Size4KiB> = os_units::NumOfPages<T>;
 
 use x86_64::structures::paging::Size4KiB;
-
-pub(crate) use allocator::Allocator;
-pub use exit_boot_services::exit_boot_services_and_return_mmap;
-pub(crate) use mapper::Mapper;
-pub use system_table::SystemTable;
+pub(crate) use {allocator::Allocator, mapper::Mapper};
+pub use {exit_boot_services::exit_boot_services_and_return_mmap, system_table::SystemTable};
