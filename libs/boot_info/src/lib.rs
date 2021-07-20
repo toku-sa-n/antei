55178fa36655afe2ab00ea2303fa5cdeee1e107e
Hiroki Tokunaga <tokusan441@gmail.com>
2021-07-20 11:21:49 +0900

    feat(BootInfo): add the `mmap` method (#117)

--- libs/boot_info/src/lib.rs
+++ libs/boot_info/src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 
-use x86_64::VirtAddr;
+use {aligned_ptr::slice, uefi_wrapper::service::boot::MemoryDescriptor, x86_64::VirtAddr};
 
 const MAGIC_HEADER: u64 = 0x0114_0514_1919_0810;
 const MAGIC_FOOTER: u64 = 0x0334_0072_dead_cafe;
@@ -10,19 +10,17 @@ const MAGIC_FOOTER: u64 = 0x0334_0072_dead_cafe;
 pub struct BootInfo {
     magic_header: u64,
 
-    mmap_start: VirtAddr,
-    mmap_len: usize,
+    mmap: Mmap,
 
     magic_footer: u64,
 }
 impl BootInfo {
     #[must_use]
-    pub fn new(mmap_start: VirtAddr, mmap_len: usize) -> Self {
+    pub fn new(mmap: Mmap) -> Self {
         Self {
             magic_header: MAGIC_HEADER,
 
-            mmap_start,
-            mmap_len,
+            mmap,
 
             magic_footer: MAGIC_FOOTER,
         }
@@ -31,6 +29,10 @@ impl BootInfo {
     pub fn check_header_and_footer(&self) {
         self.check_header();
         self.check_footer();
+    }
+
+    pub fn mmap_mut(&mut self) -> &mut Mmap {
+        &mut self.mmap
     }
 
     fn check_header(&self) {
@@ -45,5 +47,27 @@ impl BootInfo {
             self.magic_footer, MAGIC_FOOTER,
             "Invalid boot information footer."
         );
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+pub struct Mmap {
+    start: VirtAddr,
+    len: usize,
+}
+impl Mmap {
+    /// # Safety
+    ///
+    /// An array of the type [`MemoryDescriptor`] whose len is `len` must exist from
+    /// `start`.
+    #[must_use]
+    pub unsafe fn new(start: VirtAddr, len: usize) -> Self {
+        Self { start, len }
+    }
+
+    pub fn as_slice_mut(&mut self) -> &mut [MemoryDescriptor] {
+        // SAFETY: `Mmap::new` ensures the safety.
+        unsafe { slice::from_raw_parts_mut(self.start.as_mut_ptr(), self.len) }
     }
 }
