1802ec087c5b0cf3107e03d5bf989d9ce38749fb
toku-sa-n <tokusan441@gmail.com>
2021-07-22 21:59:34 +0900

    chore: switch the kernel stack
    The main goal is to place all kernel things above the virtual address `0xffff_ffff_8000_0000`. Putting all kernel things above the address makes it easier to find a bug because touching memory below the address will cause a page fault.

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,12 +5,15 @@
 extern crate kernel as _;
 
 use {
+    aligned_ptr::ptr,
     boot_info::BootInfo,
     kernel::{fini, gdt, idt, mem},
 };
 
 #[no_mangle]
-extern "sysv64" fn main(mut boot_info: BootInfo) {
+extern "sysv64" fn main(boot_info: *mut BootInfo) {
+    // SAFETY: `boot_info` is the pointer passed from the bootloader. w
+    let mut boot_info = unsafe { ptr::get(boot_info) };
     boot_info.validate();
 
     mem::phys::init(boot_info.mmap_mut().as_slice_mut());
