689dbae76af2bed9e16d45197f2d7dabd20a55c9
toku-sa-n <tokusan441@gmail.com>
2021-07-22 23:26:57 +0900

    chore(kernel): unmap all user pages

--- kernel/src/mem/pml4.rs
+++ kernel/src/mem/pml4.rs
@@ -1,7 +1,7 @@
 use {
     aligned_ptr::ptr,
     conquer_once::spin::OnceCell,
-    spinning_top::Spinlock,
+    spinning_top::{Spinlock, SpinlockGuard},
     x86_64::{structures::paging::RecursivePageTable, VirtAddr},
 };
 
@@ -14,6 +14,11 @@ static PML4: OnceCell<Spinlock<RecursivePageTable<'_>>> = OnceCell::uninit();
 /// - The recursive address `0xff7f_bfdf_e000` must point to the current working PML4.
 /// - There must not exist any references that point to the current working PML4.
 pub unsafe fn init() {
+    init_static();
+    unmap_all_user_regions();
+}
+
+fn init_static() {
     // SAFETY: The caller must ensure that the recursive paging address must point to the current
     // working PML4.
     let working_pml4 = unsafe { ptr::as_mut(RECURSIVE_ADDR.as_mut_ptr()) };
@@ -23,4 +28,21 @@ pub unsafe fn init() {
 
     let r = PML4.try_init_once(|| Spinlock::new(working_pml4));
     r.expect("Failed to initialize a reference to PML4.");
+}
+
+fn unmap_all_user_regions() {
+    let mut pml4 = pml4();
+    let pml4 = pml4.level_4_table();
+
+    for i in 0..510 {
+        pml4[i].set_unused();
+    }
+}
+
+fn pml4<'a>() -> SpinlockGuard<'a, RecursivePageTable<'static>> {
+    let pml4 = PML4.try_get();
+    let pml4 = pml4.expect("`pml4::init` is not called.");
+    let pml4 = pml4.try_lock();
+
+    pml4.expect("Failed to acquire kernel's PML4.")
 }
