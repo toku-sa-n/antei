5b3898e3f8c24d7bf03e262a5f910244bb0ca283
toku-sa-n <tokusan441@gmail.com>
2021-07-22 22:14:54 +0900

    chore(kernel): mark `main` as unsafe

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -10,8 +10,11 @@ use {
     kernel::{fini, gdt, idt, mem},
 };
 
+/// # Safety
+///
+/// `boot_info` must be dereferenceable.
 #[no_mangle]
-extern "sysv64" fn main(boot_info: *mut BootInfo) {
+unsafe extern "sysv64" fn main(boot_info: *mut BootInfo) {
     // SAFETY: `boot_info` is the pointer passed from the bootloader. w
     let mut boot_info = unsafe { ptr::get(boot_info) };
     boot_info.validate();
