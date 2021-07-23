cedcf01032b79a52d50db2e2a01079342cac1431
toku-sa-n <tokusan441@gmail.com>
2021-07-23 14:19:18 +0900

    refactor(mapping): use the default generic type

--- kernel/src/mem/mapping.rs
+++ kernel/src/mem/mapping.rs
@@ -1,19 +1,19 @@
 use {
     super::{frame_allocator, mapper},
-    x86_64::structures::paging::{Mapper, Page, PageTableFlags, PhysFrame, Size4KiB},
+    x86_64::structures::paging::{Mapper, Page, PageTableFlags, PhysFrame},
 };
 
 /// # Safety
 ///
 /// See: [`Mapper::map_to`].
-pub unsafe fn map_to(page: Page<Size4KiB>, frame: PhysFrame<Size4KiB>, flags: PageTableFlags) {
+pub unsafe fn map_to(page: Page, frame: PhysFrame, flags: PageTableFlags) {
     let f = unsafe { mapper().map_to(page, frame, flags, &mut *frame_allocator()) };
     let f = f.expect("Failed to map a page.");
 
     f.flush();
 }
 
-pub fn unmap(page: Page<Size4KiB>) {
+pub fn unmap(page: Page) {
     let r = mapper().unmap(page);
 
     let (_, f) = r.expect("Failed to unmap a page.");
