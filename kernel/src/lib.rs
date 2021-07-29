05f6b05a319d343844844fa3e3859ac0c022b17c
Hiroki Tokunaga <tokusan441@gmail.com>
2021-07-29 15:57:34 +0900

    refactor(kernel): define `init` (#158)

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -9,10 +9,23 @@ pub mod mem;
 
 #[cfg(test_on_qemu)]
 use x86_64::structures::paging::Size4KiB;
-use {core::panic::PanicInfo, qemu_print::qemu_println};
+use {aligned_ptr::ptr, boot_info::BootInfo, core::panic::PanicInfo, qemu_print::qemu_println};
 
 #[cfg(test_on_qemu)]
 pub(crate) type NumOfPages<T = Size4KiB> = os_units::NumOfPages<T>;
+
+pub fn init(boot_info: &mut BootInfo) {
+    // SAFETY: `boot_info` is the pointer passed from the bootloader. w
+    let mut boot_info = unsafe { ptr::get(boot_info) };
+    boot_info.validate();
+
+    // SAFETY: The recursive address is accessible and there are no references to the current
+    // working PML4.
+    unsafe { mem::init(boot_info.mmap().as_slice()) };
+
+    gdt::init();
+    idt::init();
+}
 
 #[cfg(test_on_qemu)]
 pub fn fini() -> ! {
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -7,7 +7,7 @@ extern crate kernel as _;
 use {
     aligned_ptr::ptr,
     boot_info::BootInfo,
-    kernel::{fini, gdt, idt, mem},
+    kernel::{fini, init},
 };
 
 /// # Safety
@@ -15,16 +15,8 @@ use {
 /// `boot_info` must be dereferenceable.
 #[no_mangle]
 unsafe extern "sysv64" fn main(boot_info: *mut BootInfo) {
-    // SAFETY: `boot_info` is the pointer passed from the bootloader. w
-    let mut boot_info = unsafe { ptr::get(boot_info) };
-    boot_info.validate();
-
-    // SAFETY: The recursive address is accessible and there are no references to the current
-    // working PML4.
-    unsafe { mem::init(boot_info.mmap().as_slice()) };
-
-    gdt::init();
-    idt::init();
+    // SAFETY: `boot_info` must be dereferenceable.
+    init(unsafe { ptr::as_mut(boot_info) });
 
     fini();
 }
