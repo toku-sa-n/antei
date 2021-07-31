26293eb5249cdabae2a6d8a4f6a71d4c67b0a060
Hiroki Tokunaga <tokusan441@gmail.com>
2021-07-31 12:59:45 +0900

    refactor(virt): use `and_then`

--- kernel/src/mem/virt.rs
+++ kernel/src/mem/virt.rs
@@ -90,7 +90,7 @@ unsafe fn try_map_frame_range_from_page_range(
     let n = frame_range.end - frame_range.start;
     let n = NumOfPages::new(n.try_into().unwrap());
 
-    find_unused_page_range_from_range(n, page_range).map_or(None, |page_range| {
+    find_unused_page_range_from_range(n, page_range).and_then(|page_range| {
         unsafe { map_range(page_range, frame_range) };
         Some(page_range)
     })
