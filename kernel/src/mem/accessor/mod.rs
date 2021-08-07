aa804f6930c158c141072b3743ecf019968b9e3f
Hiroki Tokunaga <tokusan441@gmail.com>
2021-08-08 08:52:12 +0900

    refactor(kernel): define as a function
    A method which does not have the `self` parameter can be defined as a
    function.

--- kernel/src/mem/accessor/mod.rs
+++ kernel/src/mem/accessor/mod.rs
@@ -18,24 +18,13 @@ use {
 };
 
 pub(crate) struct Mapper;
-impl Mapper {
-    fn map_from_phys_addr_and_bytes(&self, p: PhysAddr, b: Bytes) -> VirtAddr {
-        let frame_range = to_frame_range(p, b.as_num_of_pages());
-
-        let page_range = unsafe {
-            virt::map_frame_range_from_page_range(kernel_mmap::kernel_dma(), frame_range)
-        };
-
-        page_range.start.start_address() + p.as_u64() % Size4KiB::SIZE
-    }
-}
 impl accessor::Mapper for Mapper {
     unsafe fn map(&mut self, phys_start: usize, bytes: usize) -> NonZeroUsize {
         let p = PhysAddr::new(phys_start.try_into().unwrap());
 
         let b = Bytes::new(bytes);
 
-        let v = self.map_from_phys_addr_and_bytes(p, b);
+        let v = map_from_phys_addr_and_bytes(p, b);
 
         NonZeroUsize::new(v.as_u64().try_into().unwrap()).unwrap()
     }
@@ -46,6 +35,15 @@ impl accessor::Mapper for Mapper {
 
         unmap_from_virt_addr_and_bytes(v, b);
     }
+}
+
+fn map_from_phys_addr_and_bytes(p: PhysAddr, b: Bytes) -> VirtAddr {
+    let frame_range = to_frame_range(p, b.as_num_of_pages());
+
+    let page_range =
+        unsafe { virt::map_frame_range_from_page_range(kernel_mmap::kernel_dma(), frame_range) };
+
+    page_range.start.start_address() + p.as_u64() % Size4KiB::SIZE
 }
 
 fn unmap_from_virt_addr_and_bytes(v: VirtAddr, b: Bytes) {
