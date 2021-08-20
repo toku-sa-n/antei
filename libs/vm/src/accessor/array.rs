084aa353f31a196fb111e05597288dd06a4b39c6
Hiroki Tokunaga <tokusan441@gmail.com>
2021-08-20 09:50:03 +0900

    chore(accessor): remove `Copy` trait bound (#196)
    This trait bound is no longer necessary.

--- libs/vm/src/accessor/array.rs
+++ libs/vm/src/accessor/array.rs
@@ -11,7 +11,7 @@ pub type WriteOnly<T> = Generic<T, marker::WriteOnly>;
 /// Refer to [`accessor::single::ReadWrite::new`].
 #[cfg_attr(target_pointer_width = "64", allow(clippy::missing_panics_doc))]
 #[must_use]
-pub unsafe fn read_write<T: Copy>(p: PhysAddr, len: usize) -> ReadWrite<T> {
+pub unsafe fn read_write<T>(p: PhysAddr, len: usize) -> ReadWrite<T> {
     // SAFETY: The caller must uphold the safety requirements.
     unsafe { ReadWrite::new(p.as_u64().try_into().unwrap(), len, Mapper) }
 }
@@ -21,7 +21,7 @@ pub unsafe fn read_write<T: Copy>(p: PhysAddr, len: usize) -> ReadWrite<T> {
 /// Refer to [`accessor::single::ReadOnly::new`].
 #[cfg_attr(target_pointer_width = "64", allow(clippy::missing_panics_doc))]
 #[must_use]
-pub unsafe fn read_only<T: Copy>(p: PhysAddr, len: usize) -> ReadOnly<T> {
+pub unsafe fn read_only<T>(p: PhysAddr, len: usize) -> ReadOnly<T> {
     // SAFETY: The caller must uphold the safety requirements.
     unsafe { ReadOnly::new(p.as_u64().try_into().unwrap(), len, Mapper) }
 }
@@ -31,7 +31,7 @@ pub unsafe fn read_only<T: Copy>(p: PhysAddr, len: usize) -> ReadOnly<T> {
 /// Refer to [`accessor::single::WriteOnly::new`].
 #[cfg_attr(target_pointer_width = "64", allow(clippy::missing_panics_doc))]
 #[must_use]
-pub unsafe fn write_only<T: Copy>(p: PhysAddr, len: usize) -> WriteOnly<T> {
+pub unsafe fn write_only<T>(p: PhysAddr, len: usize) -> WriteOnly<T> {
     // SAFETY: The caller must uphold the safety requirements.
     unsafe { WriteOnly::new(p.as_u64().try_into().unwrap(), len, Mapper) }
 }
