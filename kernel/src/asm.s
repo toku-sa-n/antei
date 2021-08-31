0c4f59c97d9817d085229a2a4788491585715256
Hiroki Tokunaga <tokusan441@gmail.com>
2021-08-31 20:17:29 +0900

    style(asm.s): apply `asmfmt` (#214)

--- kernel/src/asm.s
+++ kernel/src/asm.s
@@ -1,52 +1,52 @@
-    .text
-    .code64
-    .intel_syntax noprefix
+.text
+.code64
+.intel_syntax noprefix
 
+.macro  handler vector
+.extern interrupt_handler_\vector
+.global asm_interrupt_handler_\vector
 
-    .macro handler vector
-    .extern interrupt_handler_\vector
-    .global asm_interrupt_handler_\vector
 asm_interrupt_handler_\vector:
-    push rbp
-    mov rbp, rsp
+	push rbp
+	mov  rbp, rsp
 
-    push rax
-    push rcx
-    push rdx
-    push rsi
-    push rdi
-    push r8
-    push r9
-    push r10
-    push r11
+	push rax
+	push rcx
+	push rdx
+	push rsi
+	push rdi
+	push r8
+	push r9
+	push r10
+	push r11
 
-    // `fxsave` saves 512-byte data, and it requires a 16-byte aligned address.
-    // After the `call` instruction, `rsp mod 16` is 8, so we add `8` here.
-    sub rsp, 512+8
+	// `fxsave` saves 512-byte data, and it requires a 16-byte aligned address.
+	// After the `call` instruction, `rsp mod 16` is 8, so we add `8` here.
+	sub rsp, 512+8
 
-    fxsave [rsp]
+	fxsave [rsp]
 
-    call interrupt_handler_\vector
+	call interrupt_handler_\vector
 
-    fxrstor [rsp]
+	fxrstor [rsp]
 
-    add rsp, 512+8
+	add rsp, 512+8
 
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rdi
-    pop rsi
-    pop rdx
-    pop rcx
-    pop rax
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rax
 
-    mov rsp, rbp
-    pop rbp
+	mov rsp, rbp
+	pop rbp
 
-    iretq
-.endm
+	iretq
+	.endm
 
-    handler 0x0e
-    handler 0x20
+	handler 0x0e
+	handler 0x20
