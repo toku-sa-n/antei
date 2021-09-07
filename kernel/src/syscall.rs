5c1afebfa33abdfde3e2ead0d2a1fc2d97e240f9
Hiroki Tokunaga <tokusan441@gmail.com>
2021-09-07 20:15:39 +0900

    chore: disable interrupts on system calls

--- kernel/src/syscall.rs
+++ kernel/src/syscall.rs
@@ -4,11 +4,14 @@ use {
     x86_64::{
         registers::{
             control::{Efer, EferFlags},
-            model_specific::{LStar, Star},
+            model_specific::{LStar, Msr, Star},
+            rflags::RFlags,
         },
         VirtAddr,
     },
 };
+
+const IA32_FMASK: Msr = Msr::new(0xc000_0084);
 
 pub(super) fn init() {
     register_handler();
@@ -20,6 +23,8 @@ pub(super) fn init() {
     unsafe {
         enable_syscall_and_sysret();
     }
+
+    disable_interrupts_on_syscall();
 }
 
 /// # Safety
@@ -51,4 +56,44 @@ fn register_segments_with_star() {
         gdt::kernel_data_selector(),
     );
     r.expect("Failed to register segment registers with STAR.");
+}
+
+fn disable_interrupts_on_syscall() {
+    // SAFETY: Disabling interrupts on a system call does not violate memory safety.
+    unsafe {
+        update_ia32_fmask(|mask| mask.insert(RFlags::INTERRUPT_FLAG));
+    }
+}
+
+/// # Safety
+///
+/// See: [`x86_64::registers::rflags::write`].
+unsafe fn update_ia32_fmask(f: impl FnOnce(&mut RFlags)) {
+    let mut mask = read_ia32_fmask();
+
+    f(&mut mask);
+
+    // SAFETY: The caller must uphold the safety requirements.
+    unsafe {
+        write_ia32_fmask(mask);
+    }
+}
+
+fn read_ia32_fmask() -> RFlags {
+    // SAFETY: Reading from IA32_FMASK does not violate memory safety.
+    let mask = unsafe { IA32_FMASK.read() };
+    let mask = RFlags::from_bits(mask);
+    mask.expect("Invalid rflags.")
+}
+
+/// # Safety
+///
+/// See [`x86_64::registers::rflag::write`].
+unsafe fn write_ia32_fmask(mask: RFlags) {
+    // SAFETY: The caller must uphold the safety requirements.
+    unsafe {
+        let mut reg = IA32_FMASK;
+
+        reg.write(mask.bits());
+    }
 }
