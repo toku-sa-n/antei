db6e3b22aebe401cb2fb635bbb92f13bf61f3dd0
Hiroki Tokunaga <tokusan441@gmail.com>
2021-09-11 21:32:29 +0900

    chore: define prototypes

--- libs/driver/src/lib.rs
+++ libs/driver/src/lib.rs
@@ -1,7 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![no_std]
+
+use ipc::Message;
+
+pub trait Handler {
+    fn init(&mut self);
+    fn open(&mut self, message: &Message);
+    fn close(&mut self, message: &Message);
+    fn read(&mut self, message: &Message);
+    fn write(&mut self, message: &Message);
+    fn ioctl(&mut self, message: &Message);
+}
+
+pub fn run(mut handler: impl Handler) -> ! {
+    handler.init();
+
+    main_loop(handler);
+}
+
+fn main_loop(mut handler: impl Handler) -> ! {
+    loop {
+        loop_iteration(&mut handler);
     }
 }
+
+fn loop_iteration(_handler: &mut impl Handler) {}
