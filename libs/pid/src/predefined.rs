9b231f515fea3bcdbabcf4648cc5f1f1cf36c30d
Hiroki Tokunaga <tokusan441@gmail.com>
2021-09-12 18:24:47 +0900

    chore: add vm

--- libs/pid/src/predefined.rs
+++ libs/pid/src/predefined.rs
@@ -4,3 +4,4 @@ pub const IDLE: Pid = Pid::new(0);
 pub const INIT: Pid = Pid::new(1);
 pub const SYSPROC: Pid = Pid::new(2);
 pub const PM: Pid = Pid::new(3);
+pub const VM_SERVER: Pid = Pid::new(4);
