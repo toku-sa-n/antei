81375ed6c0ffb8c62a106272d288401122e29d7a
Hiroki Tokunaga <tokusan441@gmail.com>
2021-09-13 20:02:41 +0900

    refactor: remove unused imports

--- servers/init/src/main.rs
+++ servers/init/src/main.rs
@@ -3,14 +3,6 @@
 
 extern crate init as _;
 
-use {
-    ipc::{
-        message::{Body, Header, Message},
-        ReceiveFrom,
-    },
-    pid::{predefined, Pid},
-};
-
 #[no_mangle]
 fn main() -> ! {
     loop {
