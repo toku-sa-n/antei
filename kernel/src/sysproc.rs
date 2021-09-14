fc720322942e1597d9dd63cbc7bc46f5e4811381
Hiroki Tokunaga <tokusan441@gmail.com>
2021-09-14 17:59:41 +0900

    refactor: define `handle_copy_data_from`

--- kernel/src/sysproc.rs
+++ kernel/src/sysproc.rs
@@ -22,35 +22,37 @@ fn loop_iteration() {
 
     match FromPrimitive::from_u64(message.body.0) {
         Some(syscalls::Ty::Noop) => reply_ack(message.header.sender_pid),
-        Some(syscalls::Ty::CopyDataFrom) => {
-            let src_pid = Pid::new(message.body.1.try_into().unwrap());
-            let src_addr = VirtAddr::new(message.body.2);
-            let dst_addr = VirtAddr::new(message.body.3);
-            let bytes = Bytes::new(message.body.4.try_into().unwrap());
-
-            // TODO: Remove this limitation.
-            assert!(bytes.as_usize() < 128, "`bytes` must be less than 128.");
-
-            let data = process::enter_address_space_and_do(src_pid, || {
-                let mut buffer = [0_u8; 128];
-
-                for (i, b) in buffer.iter_mut().enumerate().take(bytes.as_usize()) {
-                    *b = unsafe { (src_addr + i).as_ptr::<u8>().read() };
-                }
-
-                buffer
-            });
-
-            for (i, &d) in data.iter().enumerate().take(bytes.as_usize()) {
-                unsafe {
-                    (dst_addr + i).as_mut_ptr::<u8>().write(d);
-                }
-            }
-
-            reply_ack(message.header.sender_pid);
-        }
+        Some(syscalls::Ty::CopyDataFrom) => handle_copy_data_from(&message),
         None => log::warn!("Unrecognized message: {:?}", message),
     }
+}
+
+fn handle_copy_data_from(message: &Message) {
+    let src_pid = Pid::new(message.body.1.try_into().unwrap());
+    let src_addr = VirtAddr::new(message.body.2);
+    let dst_addr = VirtAddr::new(message.body.3);
+    let bytes = Bytes::new(message.body.4.try_into().unwrap());
+
+    // TODO: Remove this limitation.
+    assert!(bytes.as_usize() < 128, "`bytes` must be less than 128.");
+
+    let data = process::enter_address_space_and_do(src_pid, || {
+        let mut buffer = [0_u8; 128];
+
+        for (i, b) in buffer.iter_mut().enumerate().take(bytes.as_usize()) {
+            *b = unsafe { (src_addr + i).as_ptr::<u8>().read() };
+        }
+
+        buffer
+    });
+
+    for (i, &d) in data.iter().enumerate().take(bytes.as_usize()) {
+        unsafe {
+            (dst_addr + i).as_mut_ptr::<u8>().write(d);
+        }
+    }
+
+    reply_ack(message.header.sender_pid);
 }
 
 fn reply_ack(to: Pid) {
