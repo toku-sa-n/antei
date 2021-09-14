6c7e2807e376ac41fec44c6861cf067ee3e959f4
Hiroki Tokunaga <tokusan441@gmail.com>
2021-09-14 19:23:27 +0900

    chore: implement the `copy_data_from` system call (#298)

--- kernel/src/sysproc.rs
+++ kernel/src/sysproc.rs
@@ -1,8 +1,14 @@
 use {
-    crate::process::ipc::{receive, send, ReceiveFrom},
-    core::mem::MaybeUninit,
+    crate::process::{
+        self,
+        ipc::{receive, send, ReceiveFrom},
+    },
+    core::{convert::TryInto, mem::MaybeUninit, ptr},
     ipc_api::message::Message,
     num_traits::FromPrimitive,
+    os_units::Bytes,
+    pid::Pid,
+    x86_64::VirtAddr,
 };
 
 pub(crate) fn main() -> ! {
@@ -14,14 +20,42 @@ pub(crate) fn main() -> ! {
 fn loop_iteration() {
     let message = receive_message();
 
-    if let Some(syscalls::Ty::Noop) = FromPrimitive::from_u64(message.body.0) {
-        let to = message.header.sender_pid;
-
-        let r = send(to, Message::default());
-        r.unwrap_or_else(|_| log::warn!("Failed to send a message to {}", to));
-    } else {
-        log::warn!("Unrecognized message: {:?}", message);
+    match FromPrimitive::from_u64(message.body.0) {
+        Some(syscalls::Ty::Noop) => reply_ack(message.header.sender_pid),
+        Some(syscalls::Ty::CopyDataFrom) => handle_copy_data_from(&message),
+        None => log::warn!("Unrecognized message: {:?}", message),
     }
+}
+
+fn handle_copy_data_from(message: &Message) {
+    let src_pid = Pid::new(message.body.1.try_into().unwrap());
+    let src_addr = VirtAddr::new(message.body.2);
+    let dst_addr = VirtAddr::new(message.body.3);
+    let bytes = Bytes::new(message.body.4.try_into().unwrap());
+
+    // TODO: Remove this limitation.
+    assert!(bytes.as_usize() < 128, "`bytes` must be less than 128.");
+
+    let data = process::enter_address_space_and_do(src_pid, || {
+        let mut buffer = [0_u8; 128];
+
+        unsafe {
+            ptr::copy(src_addr.as_ptr(), buffer.as_mut_ptr(), bytes.as_usize());
+        }
+
+        buffer
+    });
+
+    unsafe {
+        ptr::copy(data.as_ptr(), dst_addr.as_mut_ptr(), bytes.as_usize());
+    }
+
+    reply_ack(message.header.sender_pid);
+}
+
+fn reply_ack(to: Pid) {
+    let r = send(to, Message::default());
+    r.unwrap_or_else(|_| log::warn!("Failed to send a message to {}", to));
 }
 
 fn receive_message() -> Message {
--- libs/syscalls/src/lib.rs
+++ libs/syscalls/src/lib.rs
@@ -1,9 +1,12 @@
 #![no_std]
 
 use {
+    core::convert::TryInto,
     ipc::message::{Body, Header, Message},
     num_derive::FromPrimitive,
-    pid::predefined,
+    os_units::Bytes,
+    pid::{predefined, Pid},
+    x86_64::VirtAddr,
 };
 
 /// # Panics
@@ -22,7 +25,39 @@ pub fn noop() {
     assert_eq!(reply.body, Body::default());
 }
 
+/// # Safety
+///
+/// The all arguments must be correct.
+///
+/// # Panics
+///
+/// This function panics if one of the following conditions is satisfied.
+/// - The kernel did not reply an empty message.
+/// - `bytes < 128`. This is the current implementation limitation.
+pub unsafe fn copy_data_from(src_pid: Pid, src_addr: VirtAddr, dst_addr: VirtAddr, bytes: Bytes) {
+    // TODO: Remove this limitation.
+    assert!(bytes.as_usize() < 128, "`bytes` must be less than 128.");
+
+    let message = Message {
+        header: Header::default(),
+        body: Body(
+            Ty::CopyDataFrom as _,
+            src_pid.as_usize().try_into().unwrap(),
+            src_addr.as_u64(),
+            dst_addr.as_u64(),
+            bytes.as_usize().try_into().unwrap(),
+        ),
+    };
+
+    ipc::send(predefined::SYSPROC, message);
+
+    let reply = ipc::receive(predefined::SYSPROC.into());
+
+    assert_eq!(reply.body, Body::default());
+}
+
 #[derive(Copy, Clone, Debug, FromPrimitive, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Ty {
     Noop,
+    CopyDataFrom,
 }
