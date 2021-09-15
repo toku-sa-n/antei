499b6ced88bafcdbf59dc5ee8a4e15a52e302224
Hiroki Tokunaga <tokusan441@gmail.com>
2021-09-15 13:37:50 +0900

    chore: print hello world

---
+++ drivers/tty/src/font.rs
@@ -0,0 +1,2 @@
+pub(crate) const WIDTH: u32 = 8;
+pub(crate) const HEIGHT: u32 = 8;
--- drivers/tty/src/lib.rs
+++ drivers/tty/src/lib.rs
@@ -2,6 +2,19 @@
 
 extern crate rlibc as _;
 
+mod font;
+mod vram;
+mod writer;
+
+#[doc(hidden)]
+pub use writer::_print;
+
+pub fn init() {
+    let screen_info = syscalls::get_screen_info();
+
+    vram::init(screen_info);
+}
+
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo<'_>) -> ! {
     loop {}
