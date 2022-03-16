8fcaf099e62b66464f24823b4a8d757050e598c0
Hiroki Tokunaga <tokusan441@gmail.com>
2022-03-16 10:11:57 +0900

    style: public interfaces should be first (#337)

--- libs/ipc/src/syscalls.rs
+++ libs/ipc/src/syscalls.rs
@@ -6,41 +6,6 @@ use {
     posix::sys::types::Pid as PosixPid,
 };
 
-fn execute_syscall(index: Ty, a1: u64, a2: u64) -> u64 {
-    let r: u64;
-
-    unsafe {
-        asm!("syscall",
-        inout("rdi") index as u64 => _,
-        inout("rsi") a1 => _,
-        inout("rdx") a2 => _,
-        out("rax") r,
-        out("rcx") _,
-        out("r8") _,
-        out("r9") _,
-        out("r10") _,
-        out("r11") _,
-        out("xmm0") _,
-        out("xmm1") _,
-        out("xmm2") _,
-        out("xmm3") _,
-        out("xmm4") _,
-        out("xmm5") _,
-        out("xmm6") _,
-        out("xmm7") _,
-        out("xmm8") _,
-        out("xmm9") _,
-        out("xmm10") _,
-        out("xmm11") _,
-        out("xmm12") _,
-        out("xmm13") _,
-        out("xmm14") _,
-        out("xmm15") _);
-    }
-
-    r
-}
-
 /// # Panics
 ///
 /// This function panics if there is no process with PID `to`.
@@ -112,3 +77,38 @@ impl From<Pid> for ReceiveFrom {
 
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Error;
+
+fn execute_syscall(index: Ty, a1: u64, a2: u64) -> u64 {
+    let r: u64;
+
+    unsafe {
+        asm!("syscall",
+        inout("rdi") index as u64 => _,
+        inout("rsi") a1 => _,
+        inout("rdx") a2 => _,
+        out("rax") r,
+        out("rcx") _,
+        out("r8") _,
+        out("r9") _,
+        out("r10") _,
+        out("r11") _,
+        out("xmm0") _,
+        out("xmm1") _,
+        out("xmm2") _,
+        out("xmm3") _,
+        out("xmm4") _,
+        out("xmm5") _,
+        out("xmm6") _,
+        out("xmm7") _,
+        out("xmm8") _,
+        out("xmm9") _,
+        out("xmm10") _,
+        out("xmm11") _,
+        out("xmm12") _,
+        out("xmm13") _,
+        out("xmm14") _,
+        out("xmm15") _);
+    }
+
+    r
+}
