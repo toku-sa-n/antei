9341b18fcc3b639e91c19069227a46808501c534
Hiroki Tokunaga <tokusan441@gmail.com>
2022-03-16 09:40:50 +0900

    refactor: replace with `asm!`

--- libs/ipc/src/syscalls.rs
+++ libs/ipc/src/syscalls.rs
@@ -1,13 +1,43 @@
 use {
     super::Message,
-    core::{convert::TryInto, mem::MaybeUninit},
+    core::{arch::asm, convert::TryInto, mem::MaybeUninit},
     num_derive::FromPrimitive,
     pid::Pid,
     posix::sys::types::Pid as PosixPid,
 };
 
-extern "sysv64" {
-    fn execute_syscall(index: Ty, a1: u64, a2: u64) -> u64;
+fn execute_syscall(index: Ty, a1: u64, a2: u64) -> u64 {
+    let r: u64;
+    unsafe {
+        asm!("syscall",
+        inout("rdi") index as u64 => _,
+        inout("rsi") a1 => _,
+        inout("rdx") a2 => _,
+        out("rax") r,
+        out("rcx") _,
+        out("r8") _,
+        out("r9") _,
+        out("r10") _,
+        out("r11") _,
+        out("xmm0") _,
+        out("xmm1") _,
+        out("xmm2") _,
+        out("xmm3") _,
+        out("xmm4") _,
+        out("xmm5") _,
+        out("xmm6") _,
+        out("xmm7") _,
+        out("xmm8") _,
+        out("xmm9") _,
+        out("xmm10") _,
+        out("xmm11") _,
+        out("xmm12") _,
+        out("xmm13") _,
+        out("xmm14") _,
+        out("xmm15") _);
+    }
+
+    r
 }
 
 /// # Panics
@@ -24,7 +54,7 @@ pub fn send(to: Pid, message: Message) {
 pub fn try_send(to: Pid, message: Message) -> Result<(), Error> {
     let message: *const _ = &message;
 
-    if unsafe { execute_syscall(Ty::Send, to.as_usize().try_into().unwrap(), message as _) } == 0 {
+    if execute_syscall(Ty::Send, to.as_usize().try_into().unwrap(), message as _) == 0 {
         Ok(())
     } else {
         Err(Error)
@@ -54,7 +84,7 @@ pub fn try_receive(from: ReceiveFrom) -> Result<Message, Error> {
     // negative PID is valid here because it means the sender's PID is unspecified. Also, the
     // sign information will not be lost as the kernel casts it to `i32` again.
     #[allow(clippy::cast_sign_loss)]
-    if unsafe { execute_syscall(Ty::Receive, from as _, m.as_mut_ptr() as _) } == 0 {
+    if execute_syscall(Ty::Receive, from as _, m.as_mut_ptr() as _) == 0 {
         Ok(unsafe { m.assume_init() })
     } else {
         Err(Error)
